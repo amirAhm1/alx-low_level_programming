9c248b2f5cd64ef843649a0f050226badb391f69
Amir <amirahm735@gmail.com>
2023-02-17 12:36:20 -0800

    the 2- print task

---
+++ 0x01-variables_if_else_while/2-print_alphabetic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/*betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'the third program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while 
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
