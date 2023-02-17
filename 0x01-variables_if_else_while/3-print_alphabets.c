790b8df8122c07f69d0262846dcaeea815ba3bc1
Amir <amirahm735@gmail.com>
2023-02-17 13:24:01 -0800

    3 edited task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*betty style doc for function main goes there */
 /**
  * main - Entry point
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
