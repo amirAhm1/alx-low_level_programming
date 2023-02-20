a0f2af28e120d14f79c918181661e5475014f13c
Amir <amirahm735@gmail.com>
2023-02-20 00:31:18 -0800

    6 project

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print numbers of base 10'
+ * Return: Always 0
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; m++)
+	{
+		putchar(m);
+	}
+        printf("\n");
+        return (0);
+}
