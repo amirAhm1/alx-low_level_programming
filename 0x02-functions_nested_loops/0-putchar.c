000f54cff492f3ef85e07f525e468c918ef16990
Amir <amirahm735@gmail.com>
2023-02-21 05:44:28 -0800

    the first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* function return type is void and it doesn't have parameters*/
+/**
+ * main - check the code
+ * Description: 'the third program's description'
+ * introduction - the into program
+ *
+ * Return: Always 0.
+ */
+void introduction(void)
+{
+	printf("_putchar\n");
+}
+
+int main(void)
+{
+	introduction();
+	return (0);
+}
