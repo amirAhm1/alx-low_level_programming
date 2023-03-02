02f995aad996f5d331c187286dd565e79a049107
Amir <amirahm735@gmail.com>
2023-03-02 06:20:40 -0800

    the Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/* more headers goes there */
+
+/*betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -8,15 +16,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	* write your line of code here...
-	* Remember:
-	* - you are not allowed to use a
-	* - you are not allowed to modify p
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
